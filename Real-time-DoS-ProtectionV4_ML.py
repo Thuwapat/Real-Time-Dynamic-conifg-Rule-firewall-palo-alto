a638ba55d1574b2ea18b269be71d4e64a12747dc
Thuwapat <Thuwapat.r@kkmuail.com>
2025-02-02 16:50:54 +0700

    update ML

--- Real-time-DoS-ProtectionV4_ML.py
+++ Real-time-DoS-ProtectionV4_ML.py
@@ -49,7 +49,7 @@ while True:
         feature_vector = pd.DataFrame([features])
 
         #print(ml_model.feature_names_in_)
-        #
+        
         #print(feature_vector)
 
         # Predict attack type
@@ -58,7 +58,7 @@ while True:
 
 
         # Trigger protections based on predictions
-        if predicted_attack == "1":  # DoS attack
+        if predicted_attack == 1:  # DoS attack
             print(">>>>>>>> DoS Detected by ML !!!!! <<<<<<<<")
             for src_ip, count in session_count.items():
                 src_zone, dst_zone = zone_mapping[src_ip]
@@ -66,11 +66,11 @@ while True:
                 create_dos_profile(firewall_ip, api_key, existing_rules)
                 create_dos_protection_policy(firewall_ip, api_key, src_ip, src_zone, dst_zone, rule_name, existing_rules)
 
-        elif predicted_attack == "2":  # DDoS attack
+        elif predicted_attack == 2:  # DDoS attack
             print(">>>>>>>>> DDoS Detected by ML !!!!!! <<<<<<<<")
             for src_ip, (src_zone, dst_zone) in zone_mapping.items():
                 rule_name = f"Block_Zone_{src_zone}_to_{dst_zone}"
-                create_dos_profile(api_key)
+                create_dos_profile(firewall_ip, api_key, existing_rules)
                 create_dos_protection_policy(firewall_ip, api_key, "any", src_zone, dst_zone, rule_name, existing_rules)
                 break # for stop dulicate Zone rules
 

8fd96d2c13bbe06a532490c69cc3aa255dc439fb
Thuwapat <96274678+Thuwapat@users.noreply.github.com>
2025-02-10 16:38:17 +0700

    update Ml

--- ML_forDDoS.py
+++ ML_forDDoS.py
@@ -33,7 +33,7 @@ x = combined_data.drop(columns=['label'])
 y = combined_data['label']
 
 # Split data (adjusted test size for better generalization)
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.9, stratify=y, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.75, stratify=y, random_state=42)
 
 # Optional: Feature Scaling (Uncomment if needed)
 #scaler = StandardScaler()
@@ -45,7 +45,7 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.9, stratif
 #x_test = pd.DataFrame(x_test_scaled, columns=x.columns)
 
 # Train a Random Forest Classifier (Optimized)
-rf_classifier = RandomForestClassifier(n_estimators=100, max_depth=10, max_features="sqrt",max_leaf_nodes=50, random_state=42)
+rf_classifier = RandomForestClassifier(n_estimators=100, max_depth=10, max_features="log2",max_leaf_nodes=10, random_state=42)
 rf_classifier.fit(x_train, y_train)
 
 

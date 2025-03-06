7eefe0186d8db41e5c79d5602ec8af269846c546
Thuwapat <96274678+Thuwapat@users.noreply.github.com>
2025-03-06 17:28:14 +0700

    update debug

--- Get_traffic_logs.py
+++ Get_traffic_logs.py
@@ -99,8 +99,8 @@ def get_new_traffic_logs(api_key, log_type="traffic", max_logs=100):
                 if new_logs:
                     last_receive_time = max(log['receive_time_dt'] for log in new_logs)
                     #print(f"Retrieved {len(new_logs)} new logs. Latest receive_time: {last_receive_time}")
-                else:
-                    print("No new logs found after filtering.")
+                #else:
+                    #print("No new logs found after filtering.")
                 return new_logs
             else:
                 print(f"Failed to retrieve logs: {response_xml.find('.//msg').text}")

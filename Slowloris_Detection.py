7266ba72477aed495ce459a9d08576abe7cc597d
Thuwapat <96274678+Thuwapat@users.noreply.github.com>
2025-03-06 15:12:30 +0700

    update Slowloris Char

--- Slowloris_Detection.py
+++ Slowloris_Detection.py
@@ -7,7 +7,7 @@ def detect_slowloris_from_logs(logs, threshold_matches=5, time_window=1):
     slowloris_characteristics = {
         'application': 'web-browsing',
         'repeat-count': '1',
-        'session-end-reason': 'tcp-rst-from-server',
+        'session-end-reason': ['tcp-rst-from-server', 'aged-out', 'tcp-fin'],  # เปลี่ยนเป็นลิสต์
         'subcategory-of-app': 'internet-utility',
         'category-of-app': 'general-internet',
         'technology-of-app': 'browser-based',
@@ -22,6 +22,8 @@ def detect_slowloris_from_logs(logs, threshold_matches=5, time_window=1):
         source_ip = log.get('src')
         packets_sent = int(log.get('pkts_sent', 0))
         packets_received = int(log.get('pkts_received', 0))
+        bytes_sent = int(log.get('bytes_sent', 0))
+        bytes_received = int(log.get('bytes_received', 0))
         receive_time_dt = log.get('receive_time_dt')  # ใช้ datetime object ที่แปลงแล้วจาก Get_traffic_logs.py
         
         # ตรวจสอบว่ามี receive_time_dt หรือไม่
@@ -32,7 +34,7 @@ def detect_slowloris_from_logs(logs, threshold_matches=5, time_window=1):
         matches_characteristics = (
             log.get('app') == slowloris_characteristics['application'] and
             log.get('repeatcnt') == slowloris_characteristics['repeat-count'] and
-            log.get('session_end_reason') == slowloris_characteristics['session-end-reason'] and
+            log.get('session_end_reason') in slowloris_characteristics['session-end-reason'] and  # เปลี่ยนเป็น in
             log.get('subcategory_of_app') == slowloris_characteristics['subcategory-of-app'] and
             log.get('category_of_app') == slowloris_characteristics['category-of-app'] and
             log.get('technology_of_app') == slowloris_characteristics['technology-of-app'] and
@@ -40,7 +42,9 @@ def detect_slowloris_from_logs(logs, threshold_matches=5, time_window=1):
             log.get('characteristic_of_app') == slowloris_characteristics['characteristic-of-app'] and
             log.get('tunneled_app') == slowloris_characteristics['tunneled-app'] and
             packets_sent <= 15 and
-            packets_received <= 15
+            packets_received <= 15 and
+            bytes_sent <= 5000 and
+            bytes_received <=5000
         )
         
         if matches_characteristics:
